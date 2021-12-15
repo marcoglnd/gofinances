7c2558ae7f4a686246ea80b030ff8a2732614571
marcoglnd <marco1408@gmail.com>
2021-12-15 11:30:23 -0300

    criando e posicionando cards no header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { HighlightCard } from '../../components/HighlightCard';
 
 import {
   Container,
@@ -10,6 +11,7 @@ import {
   UserName,
   UserGreeting,
   Icon,
+  HighlightCards,
 } from './styles';
 
 export function Dashboard () {
@@ -27,6 +29,11 @@ export function Dashboard () {
           <Icon name="power" />
         </UserWrapper>
       </Header>
+      <HighlightCards>
+        <HighlightCard />
+        <HighlightCard />
+        <HighlightCard />
+      </HighlightCards>
     </Container>
   );
 }
